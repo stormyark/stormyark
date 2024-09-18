8d6e3c83f540b2fd38e263c69cedba40454615df
stormy <81960990+stormyark@users.noreply.github.com>
2024-09-18 09:09:30 +0200

    Add files via upload

---
+++ assets/profile.js
@@ -0,0 +1,8 @@
+const webDeveloper = {
+  name: "stormy",
+  birth: 2005,
+  languages: ["HTML", "CSS", "JavaScript"],
+  tools: ["Docker", "Github", "LinuxServer"],
+  email: "contact@stormyark.de",
+  website: "https://stormyark.de",
+};
